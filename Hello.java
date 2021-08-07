15bb14ec1c0e772708a3df9a59b22aacc7a31444
ayhansevimli <50907556+ayhansevimli@users.noreply.github.com>
2021-08-07 21:00:54 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
+      System.out.println("Hello, World, this file is build automatically by using jenkins CI-CD tool.");
    }
 }
